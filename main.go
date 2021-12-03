bb8c047cba7e95a04142ab5bacb7f61909d0b2fb
Panco <1129443982@qq.com>
2021-12-03 11:17:18 +0800

    first

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	r := gin.Default()
+
+	if err := createDir("/app/logs"); err != nil {
+		panic(err)
+	}
+	file, err := os.Create("/app/logs/gin.log")
+	if err != nil {
+		panic(err)
+	}
+	gin.DefaultWriter = file
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8080")
+}
+
+func createDir(path string) error {
+	exists, err := pathExists(path)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		err := os.Mkdir(path, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+### Golang docker image example
+
+
+```shell
+docker-compose up
+```
